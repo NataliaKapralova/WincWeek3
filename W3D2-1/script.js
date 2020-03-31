f92dbe0600f65965ff8f9f32118a39816d3f99a5
Natalia Kapralova <n.kapralovaa@live.nl>
2020-03-31 12:07:17 +0200

    Nieuwe opdrachten

---
+++ W3D2-1/script.js
@@ -0,0 +1,24 @@
+// Opdracht W3D2-1
+
+// console.log("He hallo, ik ben eerste") 
+// setTimeout(function(){ console.log("He hallo, ik ben tweede"); }, 2000);
+// console.log("He hallo, ik ben derde");
+
+
+//Opdracht W3D2-2
+
+
+const huisWerkMaken = (vak, callback) => {
+    setTimeout(function() {
+    console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken` )
+    callback();
+    } , 2000); 
+};
+
+
+const klaarMetHuiswerk = () => { 
+console.log('Kijk mams, ik ben klaar met mijn huiswerk'); 
+}; 
+
+
+huisWerkMaken("wiskune" , klaarMetHuiswerk); 
